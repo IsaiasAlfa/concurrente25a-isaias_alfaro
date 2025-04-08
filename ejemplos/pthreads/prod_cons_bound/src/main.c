5ec68526e7b1654213447f5d6d245f7ef4e0519e
Isaias Alfaro <isaias.alfaro@ucr.ac.cr>
2025-04-07 22:24:57 -0600

    prod_cons_bound: completed documentation

--- ejemplos/pthreads/prod_cons_bound/src/main.c
+++ ejemplos/pthreads/prod_cons_bound/src/main.c
@@ -26,65 +26,121 @@ enum {
   ERR_CREATE_THREAD,
 };
 
+// datos que los hilos van a ocupar
 typedef struct {
+  // cantidad de hilos
   size_t thread_count;
+  // capacidad maxima del buffer
   size_t buffer_capacity;
+  // puntero del buffer
   double* buffer;
+  // cantidad de productos a generar
   size_t rounds;
+  // tiempos de produccion
   useconds_t producer_min_delay;
   useconds_t producer_max_delay;
   useconds_t consumer_min_delay;
   useconds_t consumer_max_delay;
 
+  // semaforos que se ocupan para controla prod_cons
   sem_t can_produce;
   sem_t can_consume;
 } shared_data_t;
 
 typedef struct {
+  // numero de hilo
   size_t thread_number;
+  // puntero a los datos compartidos
   shared_data_t* shared_data;
 } private_data_t;
 
+/**
+ * @brief Metodo de analisis de argumentos dados en la linea de terminal
+ * 
+ * @param argc cantidad de argumentos dados
+ * @param argv los argumentos brindados
+ * @param shared_data estructura donde se van a guardar
+ * @return int retorno para control de errores
+ */
 int analyze_arguments(int argc, char* argv[], shared_data_t* shared_data);
+
+/**
+ * @brief Creacion de equipos de hilos a usar
+ * 
+ * @param shared_data estructura de datos compartida que ocupa cada hilo
+ * @return int retorno para control de errores 
+ */
 int create_threads(shared_data_t* shared_data);
+
+/**
+ * @brief Metodo para la simulacion de produccion de objetos 
+ * 
+ * @param data puntero con los datos compartidos
+ */
 void* produce(void* data);
+
+/**
+ * @brief Metodo que simula la simulacion de consumir un objeto
+ * 
+ * @param data puntero con los datos compartidos
+ */
 void* consume(void* data);
+
+/**
+ * @brief Metodo para asignar un numero random para simulacion
+ * 
+ * @param min numero minimo que puede durar la simulacion
+ * @param max numero maximo que puede durar la simulacion
+ * @return useconds_t numero aleatorio entre min y max
+ */
 useconds_t random_between(useconds_t min, useconds_t max);
 
 int main(int argc, char* argv[]) {
   int error = EXIT_SUCCESS;
 
+  // asigancion de memoria para el struct de shared_data
   shared_data_t* shared_data = (shared_data_t*)
     calloc(1, sizeof(shared_data_t));
 
   if (shared_data) {
+    // analisis de argumentos
     error = analyze_arguments(argc, argv, shared_data);
     if (error == EXIT_SUCCESS) {
+      // asignacion de memoria para el buffer
       shared_data->buffer = (double*)
         calloc(shared_data->buffer_capacity, sizeof(double));
       if (shared_data->buffer) {
+        // se inicia el semaforo de produccion con el numero de buffer_capacity
         sem_init(&shared_data->can_produce, /*pshared*/ 0,
           shared_data->buffer_capacity);
+        // se incia el semaforo de consumir en 0
         sem_init(&shared_data->can_consume, /*pshared*/ 0, /*value*/ 0);
 
+        // semilla para los numero aleatorios de delay
         unsigned int seed = 0u;
         getrandom(&seed, sizeof(seed), GRND_NONBLOCK);
         srandom(seed);
 
+        // forma de mostar el tiempo de ejecucion del programa
         struct timespec start_time;
         clock_gettime(/*clk_id*/CLOCK_MONOTONIC, &start_time);
 
+        // creacion de los hilos
         error = create_threads(shared_data);
 
+        // se detiene lo que duro el programa
         struct timespec finish_time;
         clock_gettime(/*clk_id*/CLOCK_MONOTONIC, &finish_time);
 
+        // formato de tiempo y mostrarlo al usuario
         double elapsed = (finish_time.tv_sec - start_time.tv_sec) +
           (finish_time.tv_nsec - start_time.tv_nsec) * 1e-9;
         printf("execution time: %.9lfs\n", elapsed);
 
+        // destruir ambos semaforos despues de su uso
         sem_destroy(&shared_data->can_consume);
         sem_destroy(&shared_data->can_produce);
+        // liberar la memoria del buffer
         free(shared_data->buffer);
       } else {
         fprintf(stderr, "error: could not create buffer\n");
@@ -92,6 +148,7 @@ int main(int argc, char* argv[]) {
       }
     }
 
+    // liber la memoria compartida
     free(shared_data);
   } else {
     fprintf(stderr, "Error: could not allocate shared data\n");
@@ -138,6 +195,7 @@ int create_threads(shared_data_t* shared_data) {
   assert(shared_data);
   int error = EXIT_SUCCESS;
 
+  // creacion de los hilos que van a ser productor y consumidor
   pthread_t producer, consumer;
   error = pthread_create(&producer, /*attr*/ NULL, produce, shared_data);
   if (error == EXIT_SUCCESS) {
@@ -152,6 +210,7 @@ int create_threads(shared_data_t* shared_data) {
   }
 
   if (error == EXIT_SUCCESS) {
+    // join de los hilos creados
     pthread_join(producer, /*value_ptr*/ NULL);
     pthread_join(consumer, /*value_ptr*/ NULL);
   }
@@ -166,14 +225,18 @@ void* produce(void* data) {
   for (size_t round = 0; round < shared_data->rounds; ++round) {
     for (size_t index = 0; index < shared_data->buffer_capacity; ++index) {
       // wait(can_produce)
+      // semaforo que permite producir
       sem_wait(&shared_data->can_produce);
 
+      // tiempo que simula la produccion del producto
       usleep(1000 * random_between(shared_data->producer_min_delay
         , shared_data->producer_max_delay));
       shared_data->buffer[index] = ++count;
+      // indica la produccion de un producto
       printf("Produced %lg\n", shared_data->buffer[index]);
 
       // signal(can_consume)
+      // avisa que hay algo para consumir
       sem_post(&shared_data->can_consume);
     }
   }
@@ -187,14 +250,19 @@ void* consume(void* data) {
   for (size_t round = 0; round < shared_data->rounds; ++round) {
     for (size_t index = 0; index < shared_data->buffer_capacity; ++index) {
       // wait(can_consume)
+      // semaforo que permite consumir
       sem_wait(&shared_data->can_consume);
 
+      // buffer donde se encuentra el producto a consumir
       double value = shared_data->buffer[index];
+      // tiempo que simula lo que se dura en consumir
       usleep(1000 * random_between(shared_data->consumer_min_delay
         , shared_data->consumer_max_delay));
+      // indica el consumo de un producto
       printf("\tConsumed %lg\n", value);
 
       // signal(can_produce)
+      // avisa que hay espacio para producir
       sem_post(&shared_data->can_produce);
     }
   }

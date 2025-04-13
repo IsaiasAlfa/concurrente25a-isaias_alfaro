b808b15f7fa28bb3ff3c9cbdf9f15210906d352e
Isaias Alfaro <isaias.alfaro@ucr.ac.cr>
2025-04-13 01:56:12 -0600

    homework_pthread: better documentation of plate.c

--- homeworks/pthread/src/plate.c
+++ homeworks/pthread/src/plate.c
@@ -3,12 +3,14 @@
 #include <plate.h>
 
 void make_data(const char *filename, char job[], uint64_t thread_count) {
+  // memoria para las distintas estructuras
   data_job_t* data_job = (data_job_t*) calloc(1, sizeof(data_job_t));
   data_array_t* data_array = calloc(40, sizeof(struct data_array));
   shared_data_t* shared_data =
     (shared_data_t*) calloc(1, sizeof(shared_data_t));
 
   int cont_array = 0;
+  // erro al asginar memoria
   if (data_job == NULL || data_array == NULL || shared_data == NULL) {
     // Manejar errores de asignación de memoria
     fprintf(stderr, "Error al asignar memoria\n");
@@ -18,62 +20,91 @@ void make_data(const char *filename, char job[], uint64_t thread_count) {
     return;
   }
 
+  // incializar valores de shared_data
   shared_data->data_job = data_job;
   shared_data->thread_count = thread_count;
 
+  // buscar el archivo inicial y cargar todos sus datos
   jobs_find_file(filename, data_array, &cont_array);
+  // creacion de las distintas simulaciones
   make_matrix(shared_data, data_array, &cont_array, job);
 }
 
 void make_matrix(shared_data_t* shared_data, data_array_t* data_array,
     int* cont_array, char job[]) {
+  // casteo de las estructuras para un manejo mas sencillo
   data_job_t* data_job = shared_data->data_job;
+
   int position = *cont_array;
   char job_filename[104];
+
+  // crear carpeta out
   mkdir("out", 0777);
+  // crear archivo de salida
   snprintf(job_filename, sizeof(job_filename), "out/%s.tsv", job);
   FILE *job_file = fopen(job_filename, "w");
+
+  // variable para guardar hilos maximos del usuario
   uint64_t threads_max = shared_data->thread_count;
+  // for para todas las veces que se ocupan hacer distintas simulaciones
   for (int i = 0; i < position; i++) {
+    // cargar datos inciales
     plate_charge_RC(data_job, i, data_array);
     int rows = data_job->R;
     int cols = data_job->C;
+    // creacion de matriz
     heat_t** heat = matrix(rows, cols);
 
     if (heat == NULL) {
       printf("Error al asignar memoria");
       return;
     }
+    // cargar matriz
     shared_data->heat = heat;
+    // cargar datos de calor
     plate_charge_heat(i, data_array, heat);
+    // guardar cantiad maxima de hilos que pidio el usuario
     shared_data->thread_count = threads_max;
 
+    // mas hilos que filas
     if (shared_data->thread_count > data_job->R) {
       shared_data->thread_count = data_job->R;
     }
 
+    // equipo de hilos
     heat_team(shared_data);
+    // escribir el resultado
     jobs_out_file(data_array, heat, data_job, job_file, i);
+    // liberar la matriz
     free_matrix(heat, rows);
   }
+  // cerrar los archivos
   jobs_close_file(job_file);
+  // liberar la memoria
   make_free(data_job, data_array, shared_data);
 }
 
 void heat_team(shared_data_t* shared_data) {
+  // casteo de las estructuras para un manejo mas sencillo
   data_job_t* data_job = shared_data->data_job;
   heat_t** heat = shared_data->heat;
 
+  // creacion del equipo de hilos(array)
   pthread_t* threads = (pthread_t*)
     malloc(shared_data->thread_count * sizeof(pthread_t));
 
+  // memoria privada de cada hilo
   private_data_t* private_data = (private_data_t*)
     calloc(shared_data->thread_count, sizeof(private_data_t));
 
+  // ciclos que se va a ejecutar
   double cycles = 0;
+  // resta para verificar el balance
   double rest_heat = 0;
+  // variable que avisa si se llego al balance
   data_job->balance = 0;  // Inicializamos balance a 0 (no equilibrado)
 
+  // constante en la ejecucion de calor
   double burn = (data_job->time * data_job->material) /
   (data_job->area * data_job->area);
   data_job->burn = burn;
@@ -84,17 +115,21 @@ void heat_team(shared_data_t* shared_data) {
   }
 
   while (data_job->balance != 1) {
+    // suponer que se alcanzo el balance
     data_job->balance = 1;
 
     for (uint64_t thread_number = 0; thread_number < shared_data->thread_count
       ; ++thread_number) {
+      // asignacion de la memoria privada de cada hilo
       private_data[thread_number].thread_number = thread_number;
       private_data[thread_number].shared_data = shared_data;
 
+      // creacion del equipo de hilos y guardarlos en el array de trabajo
       pthread_create(&threads[thread_number], /*attr*/ NULL, make_heat
         , /*arg*/ &private_data[thread_number]);
     }
 
+    // esperar a que todos los hilos terminen de trabajar
     for (uint64_t thread_number = 0; thread_number < shared_data->thread_count
       ; ++thread_number) {
     pthread_join(threads[thread_number], /*value_ptr*/ NULL);
@@ -112,32 +147,44 @@ void heat_team(shared_data_t* shared_data) {
           heat[i][j].past_warm = heat[i][j].current_warm;
       }
     }
+    // aumentar los ciclos
     cycles++;
   }
+  // guardar la cantidad de ciclos
   data_job->report = cycles;
+  // liberar la memoriaque se utilizo para el equipo de hilos
   free(threads);
   free(private_data);
 }
 
 void* make_heat(void* data) {
+  // casteo de las estructuras para un manejo mas sencillo
   private_data_t* private_data = (private_data_t*)data;
   shared_data_t* shared_data = private_data->shared_data;
   heat_t** heat = shared_data->heat;
   data_job_t* data_job = shared_data->data_job;
 
+  // auxiliar para ver comprobar el balance
   double auxiliar = 0;
 
+  // numero propio de hilo y cantidad totales de hilos
   size_t thread_number = private_data->thread_number;
   size_t thread_count = shared_data->thread_count;
 
+  // filas que le tocan a cada hilo
   uint64_t rows_per_thread = data_job->R / thread_count;
+  // filas sobrantes por si no es divisible exacto
   uint64_t remainder = data_job->R % thread_count;
 
+  // inicio de cada hilo en la matriz
   uint64_t start = thread_number * rows_per_thread +
     (thread_number < remainder ? thread_number : remainder);
+  // fin de cada hilo en la matriz
   uint64_t end = start + rows_per_thread + (thread_number < remainder ? 1 : 0);
 
+  // caso especial primer hilo
   if (start == 0) start = 1;
+  // caso especial ultimo hilo
   if (end >= data_job->R) end = data_job->R - 1;
 
   // Iterar sobre las celdas de la matriz, excepto las fronteras
@@ -156,18 +203,21 @@ void* make_heat(void* data) {
 
 void make_free(data_job_t* data_job, data_array_t* data_array,
   shared_data_t* shared_data) {
+  // liberar la memoria de los estructs principales
   free(shared_data);
   free(data_job);
   free(data_array);
 }
 
 heat_t** matrix(int filas, int columnas) {
+  // memoria para la estructura principal
   heat_t** heat = calloc(filas, sizeof(heat_t*));
   if (heat == NULL) {
       perror("No se pudo asignar memoria para los punteros a las filas");
       exit(EXIT_FAILURE);
   }
   for (int i = 0; i < filas; i++) {
+    // memoria para las columnas
     heat[i] = malloc(columnas * sizeof(struct heat));
     if (heat[i] == NULL) {
         perror("No se pudo asignar memoria para una fila");
@@ -180,14 +230,17 @@ heat_t** matrix(int filas, int columnas) {
 }
 
 void free_matrix(heat_t** heat, int filas) {
+  // recorrer la matriz borrando las filas
   for (int i = 0; i < filas; i++) {
     free(heat[i]);
 }
+// liberar toda la estructura
 free(heat);
 }
 
 int analyze_arguments(char* filename, size_t filename_size, char* job,
     size_t job_size, uint64_t* thread_count) {
+  // variables para extrae el nombre base
   char *start, *end;
   char buffer[100];
 

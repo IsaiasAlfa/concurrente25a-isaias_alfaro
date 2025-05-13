fc4c906c987857630e4d2452a78be9144efb4d1f
Isaias Alfaro <isaias.alfaro@ucr.ac.cr>
2025-05-13 11:40:41 -0600

    Omp_team: final solution

--- omp/omp_team/src/main.cpp
+++ omp/omp_team/src/main.cpp
@@ -1,9 +1,22 @@
 // Copyright 2024 ECCI-UCR CC-BY 4.0
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-  const int thread_count = argc == 2 ? ::atoi(argv[1]) : 1;
+#include <omp.h>
 
-  std::cout << "Hello from secondary thread " << 0
-    << " of " << thread_count << std::endl;
+void heavy_task() {
+}
+
+
+int main(int argc, char* argv[]) {
+  const int thread_count = argc == 2 ? ::atoi(argv[1]) : omp_get_max_threads();
+
+  #pragma omp parallel num_threads(thread_count)
+  {
+  heavy_task();
+  #pragma omp critical(stdout)
+  std::cout << "Hello from secondary thread " << omp_get_thread_num()
+    << " of " << omp_get_num_threads() << std::endl;
+  }
+
+  std::cout << "Hello from main thread " << std::endl;
 }

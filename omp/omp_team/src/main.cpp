5587c5dce10cbc6083bab93af77d9d50b8d3c2ef
Isaias Alfaro <isaias.alfaro@ucr.ac.cr>
2025-05-16 09:33:21 -0600

    omp_team: solution whit private memory

--- omp/omp_team/src/main.cpp
+++ omp/omp_team/src/main.cpp
@@ -1,7 +1,6 @@
 // Copyright 2024 ECCI-UCR CC-BY 4.0
-#include <iostream>
-
 #include <omp.h>
+#include <iostream>
 
 void heavy_task() {
 }
@@ -10,12 +9,14 @@ void heavy_task() {
 int main(int argc, char* argv[]) {
   const int thread_count = argc == 2 ? ::atoi(argv[1]) : omp_get_max_threads();
 
-  #pragma omp parallel num_threads(thread_count)
-  {
+  #pragma omp parallel num_threads(thread_count) \
+    default(none) shared(std::cout) firstprivate(thread_count)
+  {  // NOLINT(whitespace/braces)
   heavy_task();
+
   #pragma omp critical(stdout)
   std::cout << "Hello from secondary thread " << omp_get_thread_num()
-    << " of " << omp_get_num_threads() << std::endl;
+    << " of " << thread_count << std::endl;
   }
 
   std::cout << "Hello from main thread " << std::endl;

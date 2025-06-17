c2dedf6bfb96d77359e1949ade1d32dee0953726
Isaias Alfaro <isaias.alfaro@ucr.ac.cr>
2025-06-17 11:49:21 -0600

    mpi: mpi_ping_pong final solution

--- mpi/mpi_ping_pong/src/main.cpp
+++ mpi/mpi_ping_pong/src/main.cpp
@@ -4,13 +4,18 @@
 
 #include "Mpi.hpp"
 
-void play_ping_pong(Mpi& mpi);
+void play_ping_pong(Mpi& mpi, const long delay);
 
 int main(int argc, char* argv[]) {
   try {
     Mpi mpi(argc, argv);
-    const long delay = argc >= 2 ? std::stol(argv[1]) : 500;  // milliseconds
-    play_ping_pong(mpi, delay);
+    if (mpi.size() == 2) {
+      const long delay = argc >= 2 ? std::stol(argv[1]) : 500;  // milliseconds
+      play_ping_pong(mpi, delay);
+    } else if (mpi.rank() == 0) {
+      std::cerr << "error: to much players" << std::endl;
+      std::cerr << "usage mpiexec -np 2"<< std::endl;
+    }
   } catch (const Mpi::Error& error) {
     std::cerr << "error: " << error.what() << std::endl;
   }
@@ -18,8 +23,18 @@ int main(int argc, char* argv[]) {
 }
 
 void play_ping_pong(Mpi& mpi, const long delay) {
+  long ball = 0;
+  int partner = 0;
+
+  if (mpi.rank() == 0) {
+    mpi.send(ball, mpi.rank() + 1);
+    partner = mpi.rank() + 1;
+  }
   while (true) {
-    std::cout << mpi.rank() << " serves" << std::endl;
+    mpi.receive(ball, partner);
+    ball++;
+    std::cout << mpi.rank() << " serves" << ball <<std::endl;
     usleep(1000 * delay);
+    mpi.send(ball, partner);
   }
 }

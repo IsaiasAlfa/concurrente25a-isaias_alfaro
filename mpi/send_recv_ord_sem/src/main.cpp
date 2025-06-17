811189a7a4b77e6cc9ff4471cb7a437af359dbef
Isaias Alfaro <isaias.alfaro@ucr.ac.cr>
2025-06-17 10:39:06 -0600

    mpi/send_recv_ord_sem: final solution using recv and send

--- mpi/send_recv_ord_sem/src/main.cpp
+++ mpi/send_recv_ord_sem/src/main.cpp
@@ -3,12 +3,21 @@
 
 #include "Mpi.hpp"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   try {
     Mpi mpi(argc, argv);
+    bool can_greet = false;
+    if (mpi.rank() > 0) {
+      mpi.receive(can_greet, mpi.rank() - 1);
+    }
     std::cout << "Hello from main thread of process " << mpi.rank()
-        << " of " << mpi.size() << " on " << mpi.getHostname() << std::endl;
-  } catch (const std::exception& error) {
-    std::cerr << "error: " << error.what() << std::endl;
+    << " of " << mpi.size() << " on " << mpi.getHostname() << std::endl;
+
+    if (mpi.rank() < mpi.size() - 1) {
+      mpi.send(can_greet, mpi.rank() + 1);
+    }
+  }
+  catch (const std::exception &error) {
+    std::cerr << "error: " << error.what() << std::endl; 
   }
 }
